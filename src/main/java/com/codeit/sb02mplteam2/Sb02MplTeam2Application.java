acb8ce40db332b0b24ba6afb1af0254221cce156
sangyeobhan <sangyeobhandev@gmail.com>
2025-07-29 16:42:24 +0900

    init

---
+++ src/main/java/com/codeit/sb02mplteam2/Sb02MplTeam2Application.java
@@ -0,0 +1,13 @@
+package com.codeit.sb02mplteam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb02MplTeam2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Sb02MplTeam2Application.class, args);
+  }
+
+}
