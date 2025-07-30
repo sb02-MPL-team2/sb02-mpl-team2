192ee128e50a075832aa6dc6ea6aa72d147f3226
JJHyunDev <bellhyunforgithub@gmail.com>
2025-07-30 14:38:38 +0900

    feat: RoleUpdateRequest 정의

---
+++ src/main/java/com/codeit/sb02mplteam2/domain/user/dto/RoleUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.codeit.sb02mplteam2.domain.user.dto;
+
+import com.codeit.sb02mplteam2.domain.user.entity.Role;
+import jakarta.validation.constraints.NotBlank;
+
+public record RoleUpdateRequest(
+    @NotBlank(message = "권한을 입력해주세요.")
+    Role role
+) {
+
+}
