f20b5b927b3a029141a3a385e76e44227a1e16c7
ddday366 <ddday366@gmail.com>
2025-07-31 17:28:54 +0900

    feat: dto 추가

---
+++ src/main/java/com/codeit/sb02mplteam2/domain/content/dto/ContentRequestDto.java
@@ -0,0 +1,11 @@
+package com.codeit.sb02mplteam2.domain.content.dto;
+
+import lombok.Builder;
+
+public record ContentRequestDto(
+    String title,
+    String category,
+    String description,
+    Long binaryContentId
+) {
+}
