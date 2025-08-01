13b20d9272e28067152d478c7bbcf7e228ec4eb8
JongwonLee <gudtksfl@gmail.com>
2025-08-01 16:28:08 +0900

    fix: OpenAPI 버전 수정

--- src/main/java/com/codeit/sb02mplteam2/config/OpenApiConfig.java
+++ src/main/java/com/codeit/sb02mplteam2/config/OpenApiConfig.java
@@ -4,10 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Your API Title", version = "v1"))
 @SecurityScheme(
     name = "bearerAuth",
     type = SecuritySchemeType.HTTP,
@@ -15,5 +18,16 @@ import org.springframework.context.annotation.Configuration;
     bearerFormat = "JWT"
 )
 public class OpenApiConfig {
-
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new io.swagger.v3.oas.models.info.Info()
+            .title("모두의 플리 Team2 API 문서")
+            .description("코드잇 고급 프로젝트의 Swagger API 문서입니다.")
+            .version("1.0")
+        )
+        .servers(List.of(
+            new Server().url("http://localhost:8080").description("로컬 서버")
+        ));
+  }
 }
