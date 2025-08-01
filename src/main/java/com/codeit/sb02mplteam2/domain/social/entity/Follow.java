efe9d773b80c5a9fee85bfaa9e6ffac5410c66eb
최성현 <round1126@naver.com>
2025-08-01 13:41:04 +0900

    fix: Follow 엔티티 수정

--- src/main/java/com/codeit/sb02mplteam2/domain/social/entity/Follow.java
+++ src/main/java/com/codeit/sb02mplteam2/domain/social/entity/Follow.java
@@ -1,14 +1,18 @@
 package com.codeit.sb02mplteam2.domain.social.entity;
 
 import com.codeit.sb02mplteam2.domain.user.entity.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
 import java.time.LocalDateTime;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 
 @Entity
 @NoArgsConstructor
@@ -18,19 +22,29 @@ import lombok.*;
 public class Follow {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   // 팔로우 하는 유저 (From)
-  @ManyToOne
-  @JoinColumn(name = "from_user_id")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "from_user_id", nullable = false)
   private User fromUser;
 
   // 팔로우 당하는 유저 (To)
-  @ManyToOne
-  @JoinColumn(name = "to_user_id")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "to_user_id", nullable = false)
   private User toUser;
 
+  @CreatedDate
+  @Column(name="created_at", nullable = false, updatable = false)
   private LocalDateTime createdAt;
 
+  private Follow(User follower, User followee) {
+    this.fromUser = follower;
+    this.toUser = followee;
+  }
+
+  public static Follow of(User follower, User followee) {
+    return new Follow(follower, followee);
+  }
 }
