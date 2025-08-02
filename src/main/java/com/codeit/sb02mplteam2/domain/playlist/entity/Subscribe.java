8ee6d6dedbeea15a86759618c3047c13a45c27fe
JongwonLee <gudtksfl@gmail.com>
2025-08-02 09:36:12 +0900

    refactor: CreationTimestamp 추가

--- src/main/java/com/codeit/sb02mplteam2/domain/playlist/entity/Subscribe.java
+++ src/main/java/com/codeit/sb02mplteam2/domain/playlist/entity/Subscribe.java
@@ -3,29 +3,31 @@ package com.codeit.sb02mplteam2.domain.playlist.entity;
 import com.codeit.sb02mplteam2.domain.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Subscribe {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Timestamp
+  @CreationTimestamp
   @Column(name = "subscribed_at")
   private LocalDateTime subscribedAt;
 
