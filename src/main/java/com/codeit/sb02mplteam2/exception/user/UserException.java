29dced82e8a004950cbac45fde9192e2e8c36e02
JJHyunDev <bellhyunforgithub@gmail.com>
2025-08-02 23:36:34 +0900

    feat: UserException 과 ErrorCode 추가

--- src/main/java/com/codeit/sb02mplteam2/exception/user/UserException.java
+++ src/main/java/com/codeit/sb02mplteam2/exception/user/UserException.java
@@ -2,11 +2,16 @@ package com.codeit.sb02mplteam2.exception.user;
 
 import com.codeit.sb02mplteam2.exception.ErrorCode;
 import com.codeit.sb02mplteam2.exception.MplException;
+import java.util.Map;
 
 public class UserException extends MplException {
 
   public UserException(ErrorCode errorCode) {
     super(errorCode);
+  }
+
+  public UserException(ErrorCode errorCode, Map<String, Object> details) {
+    super(errorCode, details);
   }
 
   public UserException(ErrorCode errorCode, Throwable cause) {
