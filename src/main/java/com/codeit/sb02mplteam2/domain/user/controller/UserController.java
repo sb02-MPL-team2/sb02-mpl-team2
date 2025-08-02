ee0253262bb38858cd400bf9645f2bf2675ed4ae
JJHyunDev <bellhyunforgithub@gmail.com>
2025-08-02 23:36:59 +0900

    feat: User CRUD, Controller, Service, ServiceTest 작성

---
+++ src/main/java/com/codeit/sb02mplteam2/domain/user/controller/UserController.java
@@ -0,0 +1,104 @@
+package com.codeit.sb02mplteam2.domain.user.controller;
+
+import com.codeit.sb02mplteam2.domain.user.dto.UserCreateRequest;
+import com.codeit.sb02mplteam2.domain.user.dto.UserDto;
+import com.codeit.sb02mplteam2.domain.user.dto.UserUpdateRequest;
+import com.codeit.sb02mplteam2.domain.user.service.UserService;
+import com.codeit.sb02mplteam2.security.MplUserDetails;
+import com.codeit.sb02mplteam2.swagger.UserApi;
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController implements UserApi {
+
+  private final UserService userService;
+
+  @PostMapping(value = "/users", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+  public ResponseEntity<UserDto> create(
+      @RequestPart("userCreateRequest") @Valid UserCreateRequest userCreateRequest,
+      @RequestPart(value = "profile", required = false) MultipartFile profile
+  ) {
+    log.info("사용자 생성 요청: {}", userCreateRequest);
+
+    UserDto createdUser = userService.create(userCreateRequest, Optional.ofNullable(profile));
+    log.debug("사용자 생성 응답: {}", createdUser);
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(createdUser);
+  }
+
+  @PreAuthorize("hasRole('ADMIN') or #userId == principal.id")
+  @PutMapping(value ="/users/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<UserDto> update(
+      @PathVariable("userId") Long userId,
+      @RequestPart("userUpdateRequest") @Valid UserUpdateRequest userUpdateRequest,
+      @RequestPart(value = "profile", required = false) MultipartFile profile
+  ) {
+    log.info("사용자 수정 요청: {}, {}", userId, userUpdateRequest);
+    UserDto userDto = userService.update(userId, userUpdateRequest, Optional.ofNullable(profile));
+    return ResponseEntity.status(HttpStatus.OK).body(userDto);
+  }
+
+  @GetMapping("/users/{userId}")
+  public ResponseEntity<UserDto> findById(@PathVariable Long userId) {
+    log.info("유저 조회 요청: {}", userId);
+    UserDto user = userService.findById(userId);
+    return ResponseEntity.status(HttpStatus.OK).body(user);
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @GetMapping("/users")
+  public ResponseEntity<List<UserDto>> findAll() {
+    log.info("유저 목록 조회 요청");
+    List<UserDto> users = userService.findAll();
+    return ResponseEntity.status(HttpStatus.OK).body(users);
+  }
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @DeleteMapping("/users/{userId}")
+  public ResponseEntity<Void> delete(@PathVariable Long userId) {
+    log.info("사용자 삭제 요청: {}", userId);
+    userService.softDelete(userId);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/users/me")
+  public ResponseEntity<UserDto> getMyInfo(@AuthenticationPrincipal MplUserDetails userDetails) {
+    Long myId = userDetails.getId();
+    log.info("내 정보 조회 요청: {}", myId);
+    UserDto userDto = userService.findById(myId);
+    return ResponseEntity.status(HttpStatus.OK).body(userDto);
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  @DeleteMapping("/users/me")
+  public ResponseEntity<Void> deleteMyAccount(@AuthenticationPrincipal MplUserDetails userDetails) {
+    Long myId = userDetails.getId();
+    log.info("내 계정 삭제 요청: {}", myId);
+    userService.softDelete(myId);
+    return ResponseEntity.noContent().build();
+  }
+}
--- src/main/java/com/codeit/sb02mplteam2/domain/user/service/UserService.java
+++ src/main/java/com/codeit/sb02mplteam2/domain/user/service/UserService.java
@@ -1,21 +1,21 @@
 package com.codeit.sb02mplteam2.domain.user.service;
 
-import com.codeit.sb02mplteam2.domain.binary.entity.BinaryContent;
 import com.codeit.sb02mplteam2.domain.user.dto.UserCreateRequest;
 import com.codeit.sb02mplteam2.domain.user.dto.UserDto;
 import com.codeit.sb02mplteam2.domain.user.dto.UserUpdateRequest;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
-  UserDto create(UserCreateRequest request, Optional<BinaryContent> profileCreateRequest);
+  UserDto create(UserCreateRequest request, Optional<MultipartFile> optionalMultipartFile);
 
-  UserDto find(Long userId);
+  UserDto findById(Long userId);
 
   List<UserDto> findAll();
 
   UserDto update(Long userId, UserUpdateRequest request,
-      Optional<BinaryContent> profileCreateRequest);
+      Optional<MultipartFile> optionalMultipartFile);
 
-  void delete(Long userId);
+  void softDelete(Long userId);
 }
