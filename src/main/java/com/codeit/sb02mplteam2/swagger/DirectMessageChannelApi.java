97f3dcfd0e00192385590c9dfd27554a2ef090bd
hakSick <koukai@kakao.com>
2025-08-06 11:16:13 +0900

    Update DirectMessageChannelApi.java

--- src/main/java/com/codeit/sb02mplteam2/swagger/DirectMessageChannelApi.java
+++ src/main/java/com/codeit/sb02mplteam2/swagger/DirectMessageChannelApi.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
