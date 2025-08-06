82773f7aa0cc7532042d2481536e96c6c9e5f551
hakSick <koukai@kakao.com>
2025-08-06 11:18:00 +0900

    Update DirectMessageApi.java

--- src/main/java/com/codeit/sb02mplteam2/swagger/DirectMessageApi.java
+++ src/main/java/com/codeit/sb02mplteam2/swagger/DirectMessageApi.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "DirectMessage", description = "DirectMessage API")
