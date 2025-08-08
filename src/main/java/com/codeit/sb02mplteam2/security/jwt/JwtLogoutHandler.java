f2dea51240389c06a6f930c02a7ad5089c7dad7a
JJHyunDev <bellhyunforgithub@gmail.com>
2025-08-08 22:58:04 +0900

    feat: 로그인 성공, 실패, 로그아웃 Handler 구현

---
+++ src/main/java/com/codeit/sb02mplteam2/security/jwt/JwtLogoutHandler.java
@@ -0,0 +1,40 @@
+package com.codeit.sb02mplteam2.security.jwt;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+@RequiredArgsConstructor
+public class JwtLogoutHandler implements LogoutHandler {
+
+  private final JwtService jwtService;
+
+  @Override
+  public void logout(HttpServletRequest request, HttpServletResponse response,
+      Authentication authentication) {
+    resolveRefreshToken(request)
+        .ifPresent(refreshToken -> {
+          jwtService.invalidateJwtSession(refreshToken);
+          invalidateRefreshTokenCookie(response);
+        });
+  }
+
+  private Optional<String> resolveRefreshToken(HttpServletRequest request) {
+    return Arrays.stream(request.getCookies())
+        .filter(cookie -> cookie.getName().equals(JwtService.REFRESH_TOKEN_COOKIE_NAME))
+        .findFirst()
+        .map(Cookie::getValue);
+  }
+
+  private void invalidateRefreshTokenCookie(HttpServletResponse response) {
+    Cookie refreshTokenCookie = new Cookie(JwtService.REFRESH_TOKEN_COOKIE_NAME, "");
+    refreshTokenCookie.setHttpOnly(true);
+    refreshTokenCookie.setMaxAge(0);
+    response.addCookie(refreshTokenCookie);
+  }
+}
