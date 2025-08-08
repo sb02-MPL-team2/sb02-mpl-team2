0526a70dc805211aab2d0cb38e7fa7216533b207
JJHyunDev <bellhyunforgithub@gmail.com>
2025-08-08 22:50:12 +0900

    feat: MplUserDetails, Service 수정

--- src/main/java/com/codeit/sb02mplteam2/security/MplUserDetailsService.java
+++ src/main/java/com/codeit/sb02mplteam2/security/MplUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.codeit.sb02mplteam2.security;
 
 import com.codeit.sb02mplteam2.domain.user.entity.User;
+import com.codeit.sb02mplteam2.domain.user.mapper.UserMapper;
 import com.codeit.sb02mplteam2.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class MplUserDetailsService implements UserDetailsService {
 
   private final UserRepository userRepository;
+  private final UserMapper userMapper;
 
   // Spring Security가 인증을 시도할 때 호출하는 메서드
   // 로그인 폼에서 'username'의 파라미터로 넘어온 값을 인자로 받습니다.
@@ -25,6 +27,6 @@ public class MplUserDetailsService implements UserDetailsService {
 
     // Spring Security가 이해할 수 있는 userDetails 객체로 변환하여 반환
     // return 한 MplUserDetail의 메서드를 차례로 호출해서 lock 상태인지 등을 검사한다.
-    return new MplUserDetails(user);
+    return new MplUserDetails(userMapper.toDto(user), user.getPassword());
   }
 }
