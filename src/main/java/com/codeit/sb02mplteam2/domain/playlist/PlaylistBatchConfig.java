28d012bd3d97342a92c62271708f704cdeff4b7c
JongwonLee <gudtksfl@gmail.com>
2025-08-09 14:38:58 +0900

    feat: JpaPagingItemReader로 Reader 교체

--- src/main/java/com/codeit/sb02mplteam2/domain/playlist/PlaylistBatchConfig.java
+++ src/main/java/com/codeit/sb02mplteam2/domain/playlist/PlaylistBatchConfig.java
@@ -2,15 +2,18 @@ package com.codeit.sb02mplteam2.domain.playlist;
 
 import com.codeit.sb02mplteam2.domain.playlist.entity.Playlist;
 import com.codeit.sb02mplteam2.domain.playlist.entity.PlaylistSubscriberHistory;
+import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -21,20 +24,35 @@ public class PlaylistBatchConfig {
 
   @Bean
   public Job PlaylistHistoryJob(JobRepository jobRepository, Step PlaylistHistoryStep) {
-    return new JobBuilder("playlistJob", jobRepository)
+    return new JobBuilder("PlaylistHistoryStep", jobRepository)
         .start(PlaylistHistoryStep)
+        .build();
+  }
+
+  @Bean
+  @StepScope
+  public JpaPagingItemReader<Playlist> playlistHistoryItemReader(
+      EntityManagerFactory entityManagerFactory
+  ) {
+    String jpqlQuery = "SELECT p FROM Playlist p ORDER BY p.id ASC";
+
+    return new JpaPagingItemReaderBuilder<Playlist>()
+        .name("playlistHistoryItemReader")
+        .entityManagerFactory(entityManagerFactory)
+        .queryString(jpqlQuery)
+        .pageSize(100)
         .build();
   }
 
   @Bean
   public Step PlaylistHistoryStep(JobRepository jobRepository,
       PlatformTransactionManager transactionManager,
-      ItemReader<Playlist> reader,
+      JpaPagingItemReader<Playlist> playlistHistoryItemReader,
       ItemProcessor<Playlist, PlaylistSubscriberHistory> processor,
       ItemWriter<PlaylistSubscriberHistory> writer) {
-    return new StepBuilder("playlistStep", jobRepository)
+    return new StepBuilder("PlaylistHistoryStep", jobRepository)
         .<Playlist, PlaylistSubscriberHistory>chunk(100, transactionManager)
-        .reader(reader)
+        .reader(playlistHistoryItemReader)
         .processor(processor)
         .writer(writer)
         .build();
