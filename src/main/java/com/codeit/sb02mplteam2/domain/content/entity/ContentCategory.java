99750ab29974be71b734eda1dde638b54381b494
ddday366 <ddday366@gmail.com>
2025-08-11 11:13:25 +0900

    refactor: tmdb 호출 및 Enum, Mapper 리팩토링

--- src/main/java/com/codeit/sb02mplteam2/domain/content/entity/ContentCategory.java
+++ src/main/java/com/codeit/sb02mplteam2/domain/content/entity/ContentCategory.java
@@ -1,17 +1,23 @@
 package com.codeit.sb02mplteam2.domain.content.entity;
 
 public enum ContentCategory {
-  MOVIE("/3/discover/movie"),
-  TV("/3/discover/tv"),
-  SPORTS("");
+  MOVIE("/3/discover/movie", "movie"),
+  TV("/3/discover/tv", "tv"),
+  SPORTS("", "sports");
 
   private final String path;
+  private final String metricKey;
 
-  ContentCategory(String path) {
+  ContentCategory(String path, String metricKey) {
     this.path = path;
+    this.metricKey = metricKey;
   }
 
   public String getPath() {
     return path;
+  }
+
+  public String getMetricKey() {
+    return metricKey;
   }
 }
