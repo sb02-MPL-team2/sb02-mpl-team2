580c48915142385ef12364dc63ead727da911098
ddday366 <ddday366@gmail.com>
2025-08-21 01:41:06 +0900

    refactor: 테스트 코드 및 tmdb 저장 로직 주석 처리

--- mpl-domain/src/test/java/com/codeit/sb02mplteam2/domain/content/TmdbServiceTest.java
+++ mpl-domain/src/test/java/com/codeit/sb02mplteam2/domain/content/TmdbServiceTest.java
@@ -1,45 +1,45 @@
-package com.codeit.sb02mplteam2.domain.content;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import com.codeit.sb02mplteam2.domain.content.dto.tmdb.TmdbMovieDto;
-import com.codeit.sb02mplteam2.domain.content.dto.tmdb.TmdbTvDto;
-import com.codeit.sb02mplteam2.domain.content.entity.ContentCategory;
-import com.codeit.sb02mplteam2.domain.content.service.TmdbService;
-import java.util.List;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@Disabled
-//TODO 잠시 테스트 끔
-class TmdbServiceTest {
-
-  @Autowired
-  private TmdbService tmdbService;
-
-  @Test
-  @Disabled
-  void testGetTmdbMoviesRealApi() {
-    List<TmdbMovieDto> movies = tmdbService.getTmdbMovies(ContentCategory.MOVIE);
-
-    assertThat(movies).isNotEmpty();
-    movies.stream()
-        .limit(5)
-        .forEach(m -> System.out.println(m.title() + " | " + m.release_date()));
-  }
-
-  @Test
-  @Disabled
-  void testGetTmdbTvsRealApi() {
-    List<TmdbTvDto> tvs = tmdbService.getTmdbTvs(ContentCategory.TV);
-
-    assertThat(tvs).isNotEmpty();
-    tvs.stream()
-        .limit(5)
-        .forEach(t -> System.out.println(t.name() + " | " + t.first_air_date()));
-  }
-
-}
+//package com.codeit.sb02mplteam2.domain.content;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//import com.codeit.sb02mplteam2.domain.content.dto.tmdb.TmdbMovieDto;
+//import com.codeit.sb02mplteam2.domain.content.dto.tmdb.TmdbTvDto;
+//import com.codeit.sb02mplteam2.domain.content.entity.ContentCategory;
+//import com.codeit.sb02mplteam2.domain.content.service.TmdbService;
+//import java.util.List;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//@Disabled
+////TODO 잠시 테스트 끔
+//class TmdbServiceTest {
+//
+//  @Autowired
+//  private TmdbService tmdbService;
+//
+//  @Test
+//  @Disabled
+//  void testGetTmdbMoviesRealApi() {
+//    List<TmdbMovieDto> movies = tmdbService.getTmdbMovies(ContentCategory.MOVIE);
+//
+//    assertThat(movies).isNotEmpty();
+//    movies.stream()
+//        .limit(5)
+//        .forEach(m -> System.out.println(m.title() + " | " + m.release_date()));
+//  }
+//
+//  @Test
+//  @Disabled
+//  void testGetTmdbTvsRealApi() {
+//    List<TmdbTvDto> tvs = tmdbService.getTmdbTvs(ContentCategory.TV);
+//
+//    assertThat(tvs).isNotEmpty();
+//    tvs.stream()
+//        .limit(5)
+//        .forEach(t -> System.out.println(t.name() + " | " + t.first_air_date()));
+//  }
+//
+//}
