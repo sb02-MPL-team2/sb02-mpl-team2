0a9db803235420485f0f80e57e1896efe4831913
JongwonLee <gudtksfl@gmail.com>
2025-08-27 13:23:53 +0900

    feat: 이벤트 클래스 내용 수정
    body: final 입력, NotificationEvent 내 extend 추가

--- mpl-domain/src/main/java/com/codeit/sb02mplteam2/event/BulkNotificationEvent.java
+++ mpl-domain/src/main/java/com/codeit/sb02mplteam2/event/BulkNotificationEvent.java
@@ -7,10 +7,10 @@ import org.springframework.context.ApplicationEvent;
 
 @Getter
 public class BulkNotificationEvent extends ApplicationEvent {
-  private  Set<Long> receiverIds; // 알림 받을 사용자 ID
-  private  NotificationType notificationType;
-  private  Long targetId; // 알림과 관련된 플레이리스트 ID, DM ID
-  private  Long publisherId; //이벤트를 발생시킨 사용자 ID
+  private final Set<Long> receiverIds; // 알림 받을 사용자 ID
+  private final NotificationType notificationType;
+  private final Long targetId; // 알림과 관련된 플레이리스트 ID, DM ID
+  private final Long publisherId; //이벤트를 발생시킨 사용자 ID
 
   public BulkNotificationEvent(Object source, Set<Long> receiverIds,
       NotificationType notificationType, Long targetId, Long publisherId) {
