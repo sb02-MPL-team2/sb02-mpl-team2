67ae65d71fa6ec6813709215f1999f9eaad4ba81
sangyeobhan <sangyeobhandev@gmail.com>
2025-08-30 08:00:00 +0900

    refact: OAuth 리다이렉션 로직 변경

--- mpl-domain/src/main/java/com/codeit/sb02mplteam2/config/ForwardingController.java
+++ mpl-domain/src/main/java/com/codeit/sb02mplteam2/config/ForwardingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ForwardingController {
 
-  @GetMapping(value = "/{path:^(?!api|assets|files|ws|sse|.*\\..*).*}/**")
+  @GetMapping(value = "/{path:^(?!api|assets|files|ws|sse|oauth2|.*\\..*).*}/**")
   public String forward() {
     // 서버 내부에서 /index.html 로 요청 전달
     // 브라우저의 URL은 바뀌지 않는다.
